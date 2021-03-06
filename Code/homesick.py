990bf1d748e718c04ec2202772b40b6a1a0a2211
Aryan Kafle <akafl916@gmail.com>
2021-03-06 16:42:07 -0500

    rearranged code again

--- Code/homesick.py
+++ Code/homesick.py
@@ -38,159 +38,161 @@ class Homesick(arcade.Window):
         self.stage2 = False
         self.stage3 = False
 
+        self.boxCrate = ":resources:images/tiles/boxCrate_double.png"
+
     def createMaze(self):
         for x in range(0, 800, 32):
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = x+17
             wall.center_y = 794
             self.wall_list.append(wall)
 
         for x in range(0, 800, 32):
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = x+17
             wall.center_y = 7
             self.wall_list.append(wall)
 
         for y in range(80, 800, 32):
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = 795
             wall.center_y = y+20
             self.wall_list.append(wall)
             
         for y in range(0, 800, 32):
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = 4
             wall.center_y = y+17
             self.wall_list.append(wall)
         
         for x in range(0, 350, 32): #1
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = x+15
             wall.center_y = 700
             self.wall_list.append(wall)
 
         for x in range(0, 350, 32): #2
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = x+500
             wall.center_y = 700
             self.wall_list.append(wall)
         
         for y in range(0, 100, 32): #3
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = 350
             wall.center_y = y+605
             self.wall_list.append(wall)
 
         for y in range(0, 125, 32): #4
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = 500
             wall.center_y = y+605
             self.wall_list.append(wall)
         
         for x in range(0, 170, 32): #5
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = x+500
             wall.center_y = 580
             self.wall_list.append(wall)
         
         for y in range(450, 640, 32): #6
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = 670
             wall.center_y = y-35
             self.wall_list.append(wall)
         
         for y in range(400, 490, 32): #9
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = 297
             wall.center_y = y+15
             self.wall_list.append(wall)
         
         for x in range(370, 500, 32): #8
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = x-60
             wall.center_y = 415
             self.wall_list.append(wall)
         
         for x in range(300, 450, 32): #7
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = x+238
             wall.center_y = 415
             self.wall_list.append(wall)
         
         for x in range(180, 330, 32): #10
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = x-10
             wall.center_y = 500
             self.wall_list.append(wall)
         
         for x in range(150, 250, 32): #11
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = x-70
             wall.center_y = 585
             self.wall_list.append(wall)
         
         for y in range(0, 580, 32): #12
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = 80
             wall.center_y = y
             self.wall_list.append(wall)
         
         for y in range(150, 500, 32): #13
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = 170
             wall.center_y = y+10
             self.wall_list.append(wall)
         
         for x in range(170, 220, 32): #14
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = x
             wall.center_y = 130
             self.wall_list.append(wall)
         
         for y in range(0, 800, 32): #15
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = 4
             wall.center_y = y+17
             self.wall_list.append(wall)
         
         for x in range(330, 500, 32): #17
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = x
             wall.center_y = 290
             self.wall_list.append(wall)
         #martin
         for y in range(100, 290, 32): #18
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = 490
             wall.center_y = y
             self.wall_list.append(wall)
         #martin
         for x in range(500, 630, 32): #19
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = x
             wall.center_y = 100
             self.wall_list.append(wall)
         #martin
         for y in range(100, 250, 32): #20
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = 630
             wall.center_y = y
             self.wall_list.append(wall)
         #martin
         for y in range(100, 250, 32): #21
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = 710
             wall.center_y = y
             self.wall_list.append(wall)
         #martin
         for x in range(710, 800, 32): #22
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = x
             wall.center_y = 100
             self.wall_list.append(wall)
         #martin
         for y in range(0, 290, 32): #23
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite(self.boxCrate, SPRITE_SCALING)
             wall.center_x = 315
             wall.center_y = y
             self.wall_list.append(wall)
