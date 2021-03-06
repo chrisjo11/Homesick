aa1242f053e45057a21d592686e3b72316f73fdc
Aryan Kafle <akafl916@gmail.com>
2021-03-06 18:36:09 -0500

    organized code to work between different stages

--- Code/main.py
+++ Code/main.py
@@ -1,16 +1,20 @@
+from gamesetup import MyGame
 import arcade
-import os
-from homesick import Homesick
+
+SPRITE_SCALING = 0.25
+SPRITE_NATIVE_SIZE = 128
+SPRITE_SIZE = int(SPRITE_NATIVE_SIZE * SPRITE_SCALING)
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
-SCREEN_TITLE = "Home Sick"
+SCREEN_TITLE = "Sprite Rooms Example"
+
+MOVEMENT_SPEED = 5
 
 def main():
     """ Main method """
-    window = Homesick(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.setup()
     arcade.run()
-
-if __name__ == "__main__":
-    main()
+    
+main()
